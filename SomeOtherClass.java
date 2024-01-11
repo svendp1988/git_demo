7d77acd052ad08ad5ef9c3b7bc7233cc8f06d7ec
svendp1988 <56538162+svendp1988@users.noreply.github.com>
2024-01-11 12:00:51 +0100

    Changed SomeOtherClass.java

--- SomeOtherClass.java
+++ SomeOtherClass.java
@@ -1,5 +1,5 @@
 public class SomeOtherClass {
     public static void main(String[] args) {
-        System.out.println("Hello World Again!");
+        System.out.println("Hello World Again hello!");
     }
 }
